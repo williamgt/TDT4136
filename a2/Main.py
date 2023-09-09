ff40e7cf032bfe413834f70cc84c8de8a31c223f
William G. Tresselt <williagt@stud.ntnu.no>
2023-09-09 13:06:29 +0200

    More cleanup

--- a2/Main.py
+++ a2/Main.py
@@ -1,29 +1,31 @@
 import AStar
 import Map
 
-map = Map.Map_Obj(1)
+map1 = Map.Map_Obj(1)
 
 #map.print_map(map.str_map)
 #map.print_map(map.int_map)
 
-print(map.int_map.shape)
-print('start ' + map.start_pos)
-print('goal ' + map.goal_pos)
+#print(map1.int_map.shape)
+print('start1:')
+print(map1.start_pos)
+print('goal1:')
+print(map1.goal_pos)
 
 #Very unfinished heuristic function
 def heuristic_1(current, goal):
     return 0
 
 #Show map before shortest path
-map.show_map()
+map1.show_map()
 
-path = AStar.astar(map, map.start_pos, map.goal_pos, heuristic_1)
+path = AStar.astar(map1, map1.start_pos, map1.goal_pos, heuristic_1)
 #print(path)
 #print(len(path))
 
 #Add path to map and show it
 for pos in path:
-    map.replace_map_values(pos, 2, map.goal_pos)
+    map1.replace_map_values(pos, 2, map1.goal_pos)
 
-map.show_map()
+map1.show_map()
 
